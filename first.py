a81378eea95d6aae21612cb947cc38e57ff8e84e
AJHAMZA18 <ajhamzagithub@gmail.com>
2024-12-30 18:11:58 +0100

    first project

---
+++ first.py
@@ -0,0 +1,9 @@
+text = "programing is like building a multilingual puzzle"
+number1 =98 
+number2 =3.14159
+str = "AJ Hamza" 
+
+print(f"{text} , {number1} battery street , float: {number2}")
+print(str)
+print(str)
+print(str)
